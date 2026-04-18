bc5164320110a373c0d8d70bfbd1f9f070ac09d9
EdonitaGjyrevci <edonita.gjyrevci@student.uni-pr.edu>
2026-04-18 20:55:56 +0200

    Implementimi shtesë ne file-in e serverit(Menaxhimi i statistikave)

--- server.py
+++ server.py
@@ -4,28 +4,39 @@ import os
 import time
 import json
 
-HOST = "127.0.0.1"
+HOST = "0.0.0.0"
 PORT = 5000
 MAX_CLIENTS = 4
-TIMEOUT = 60 
+TIMEOUT = 300
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind((HOST, PORT))
-server.listen(MAX_CLIENTS)
+server.listen(5)
 
-monitoring_data = {"active_connections": 0, "message_count": 0, "all_messages": []}
+hostname = socket.gethostname()
+local_ip = socket.gethostbyname(hostname)
+
+print(f"Serveri duke punuar ne {HOST}:{PORT}")
+print(f"IP lokale (përdore për klient): {local_ip}")
+
 clients = []
 roles = {} 
 client_ids = {} 
-last_active = {} 
+sessions = {}
+last_active = {}
+
+monitoring_data = {"message_count": 0, "all_messages": []}
 
 def save_stats():
     stats = {
         "active_count": len(clients),
         "total_messages": monitoring_data["message_count"],
-        "connected_clients": [{"id": client_ids[s], "role": roles[s], "ip": s.getpeername()[0]} for s in clients if s in client_ids],
-        "logs": monitoring_data["all_messages"][-15:] 
+        "connected_clients": [
+            {"id": client_ids[s], "role": roles[s], "ip": s.getpeername()[0]} 
+            for s in clients if s in client_ids
+        ],
+        "logs": monitoring_data["all_messages"][-15:]
     }
     with open("stats.json", "w") as f:
         json.dump(stats, f, indent=4)
@@ -38,4 +49,4 @@ def disconnect(sock):
     sock.close()
     save_stats()
 
-print(f"TCP Server running on {HOST}:{PORT}...")
+print(f"Serveri po pret lidhje ne {PORT}...")
